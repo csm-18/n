5a1d78231128afb2357dac6a39b3c762c9780fba
csm-18 <112937168+csm-18@users.noreply.github.com>
2026-01-17 23:21:42 +0530

    cli: about,version,help commands

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+// n version
+const VERSION = "0.1.0"
 
 func main() {
-	fmt.Println("Got started!")
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		fmt.Println("n", VERSION)
+		fmt.Println("n programming language compiler.")
+		fmt.Println("For help:")
+		fmt.Println("  n -h")
+	} else if len(args) == 1 {
+		switch args[0] {
+		case "--version", "-v":
+			fmt.Println("n", VERSION)
+		case "--help", "-h":
+			fmt.Println("n compiler commands:")
+			fmt.Println("   n               -  show about message")
+			fmt.Println("   n -v,--version  -  show n version")
+			fmt.Println("   n -h,--help     -  show compiler commands list")
+		}
+	}
 }
