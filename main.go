41a5aad91b58f3dc074d454c3e620b29cd11ea8b
csm-18 <112937168+csm-18@users.noreply.github.com>
2026-01-18 00:04:02 +0530

    cli: init command

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 // n version
@@ -20,6 +21,8 @@ func main() {
 		switch args[0] {
 		case "--version", "-v":
 			fmt.Println("n", VERSION)
+		case "init":
+			initProject()
 		case "--help", "-h":
 			fmt.Println("n compiler commands:")
 			fmt.Println("   n               -  show about message")
@@ -27,4 +30,33 @@ func main() {
 			fmt.Println("   n -h,--help     -  show compiler commands list")
 		}
 	}
+}
+
+func initProject() {
+	module_name := filepath.Base(func() string { d, _ := os.Getwd(); return d }())
+
+	config_file_name := "n.mod"
+	config_content := "module " + module_name + "\n\n" +
+		"n " + VERSION + "\n"
+
+	config_file, err := os.OpenFile(config_file_name, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0644)
+
+	if err != nil {
+		if os.IsExist(err) {
+			fmt.Printf("Error: %s file already exists!\n", config_file_name)
+			os.Exit(1)
+		}
+		fmt.Println("Error creating file:\n", config_file_name)
+		os.Exit(1)
+	}
+	defer config_file.Close()
+
+	_, err = config_file.WriteString(config_content)
+	if err != nil {
+		fmt.Println("Error writing to file:", config_file_name)
+		os.Exit(1)
+	}
+
+	fmt.Printf("n: created new %s: module %s\n", config_file_name, module_name)
+
 }
